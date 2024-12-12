437dcbd626e096aa004bebb731f2b12916cdfa13
Vandeth <vandeth.ifl@gmail.com>
2024-12-12 17:08:55 -0600

    change the function printData() to toString()

--- src/ProblemTwo/LinkedListStack.java
+++ src/ProblemTwo/LinkedListStack.java
@@ -9,11 +9,11 @@ public class LinkedListStack {
 		Node next;
 	}
 
-	public static void push(Integer x) {
+	public void push(Integer x) {
 		if(x == null) return;
 		Node newNode = new Node();
 		newNode.value = x;
-		if(header.next == null) {
+		if(isEmpty()) {
 			header.next = newNode;
 			newNode.previous = header;
 		}
@@ -29,67 +29,72 @@ public class LinkedListStack {
 	}
 
 	//Pop the data from stack
-	static Integer pop(){
-		if(header.next == null) return null;
+	public  Integer pop(){
+		if(isEmpty()) return null;
 		Node current = header.next;
 		while (current.next != null){
 			current = current.next;
 		}
-		int n = current.value;
+		int val = current.value;
 		if (current.previous != null) {
 			current.previous.next = null;
 		} else {
 			header.next = null;
 		}
 
-		return n;
+		return val;
 	}
 
 	//Peek method implementation
-	static Integer peek(){
+	public Integer peek(){
 		Node current = header;
-		if(header.next == null) return null;
+		if(isEmpty()) return null;
 		while (current.next!= null){
 			current = current.next;
 		}
 		return current.value;
 	}
 
-	public static boolean isEmpty(){ // true if stack is empty
+	public static boolean isEmpty(){
 		return (header.next == null);
 	}
-	static void PrintData () {
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
 		Node current = header.next;
-		if (header.next == null) {
-			System.out.println("No data to print");
-			return;
-		}
+		if (isEmpty()) return "No data to print";
 		while (current != null) {
-			System.out.print(current.value);
-			System.out.print(" --->");
-			current = current.next;
-		}
-		System.out.println("null");
+				sb.append(current.value).append(" --->");
+				current = current.next;
+			}
+			sb.append("null");
+		return sb.toString();
 	}
 
 	public static void main(String[] args) {
-		push (10);
-		push (20);
-		push (30);
-		push(40);
-		push(50);
+		LinkedListStack  st = new LinkedListStack();
+		st.push (10);
+		st.push(20);
+		st.push (30);
+		st.push(40);
+		st.push(50);
 		System.out.println("Data after pushing ...");
-		PrintData();
+		System.out.println(st);
 		System.out.println("----------------------");
-		System.out.println("Pop: " + pop());
-		System.out.println("Peek: " + peek());
+		System.out.println("Pop: " + st.pop());
+		//System.out.println("Pop: " + st.pop());
+		//System.out.println("Pop: " + st.pop());
+		//System.out.println("Pop: " + st.pop());
+		//System.out.println("Pop: " + st.pop());
+		System.out.println("Peek: " + st.peek());
 		System.out.println("Data after pop then peek ...");
-		PrintData();
+		System.out.println(st);
 		System.out.println("----------------------");
-		System.out.println("Peek: " + peek());
-		System.out.println("Pop: " + pop());
+		System.out.println("Peek: " + st.peek());
+		System.out.println("Pop: " + st.pop());
 		System.out.println("Data after peek and pop ...");
-		PrintData();
+		System.out.println(st);
 		System.out.println("----------------------");
 	}
 }
